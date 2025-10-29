eb114c0ac8145d01470d4d9446a60b2e07294786
kimeunseokk <rpqhfls2whdk@gmail.com>
2025-10-29 15:11:21 +0900

    docs: README 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,14 @@
 package lotto;
+import lotto.Lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static void LottoRandomNumber(){
+        
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,8 @@ package lotto;
 
 import java.util.List;
 
+
+
 public class Lotto {
     private final List<Integer> numbers;
 
